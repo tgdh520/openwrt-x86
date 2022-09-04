fcdb7c57c2b4aef35ca1abff6ff5da3023271b8b
tgdh520 <59686937+tgdh520@users.noreply.github.com>
2022-09-04 20:07:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,8 +15,3 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
-echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
