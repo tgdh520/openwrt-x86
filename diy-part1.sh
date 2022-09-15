44c682cf78773e65ad1bbc16485320dae5b87035
tgdh520 <59686937+tgdh520@users.noreply.github.com>
2022-09-15 14:32:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,12 +11,12 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git video https://github.com/openwrt/video.git' >>feeds.conf.default
+#echo 'src-git video https://github.com/openwrt/video.git' >>feeds.conf.default
 echo 'src-git targets https://github.com/openwrt/targets.git' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
